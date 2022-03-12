80912203018bfc4df5bad5bf34159d3328ccbf26
Seraphin Siriyake <seraphinsk03@gmail.com>
2022-03-12 19:30:09 +0100

    fix file script

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// const me = {
-//     name : "seraphin" ,
-//     presentFriend : function (friend){
-//         return "Tu connais " + friend + " ?";
-//     }
-// }
-// console.log(me.presentFriend("kevin"));
-
-console.log(document.getElementById("gros"));
