6fc1ac48b48a960dfe323c298eb8cc3745c91fcb
Seraphin Siriyake <seraphinsk03@gmail.com>
2022-03-12 19:27:32 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// const me = {
+//     name : "seraphin" ,
+//     presentFriend : function (friend){
+//         return "Tu connais " + friend + " ?";
+//     }
+// }
+// console.log(me.presentFriend("kevin"));
+
+console.log(document.getElementById("gros"));
