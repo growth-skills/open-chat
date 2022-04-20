3456fbac9ae25ae9015ee2db26262cda76e655e5
Seraphin Siriyake <seraphinsk03@gmail.com>
2022-04-21 00:40:15 +0100

    new fix on html and css file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// const form = document.getElementById("form");
+
+// form.addEventListener('submit', (e) =>{
+//     const elementEmail = document.getElementById('email')
+    
+//     elementEmail.value;
+
+
+//     e.preventDefault()
+    
+// })
